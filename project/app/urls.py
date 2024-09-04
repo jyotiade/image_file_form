22552841abe0e41cf2ec75549c148819a4ca995b
jyoti ade <jyotiade287@gmail.com>
2024-09-04 18:20:56 +0530

    add to cart

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns=[
-    path("",home,name="home"),
+    path("home/",home,name="home"),
+    path("showdata/",showdata,name="showdata")
 ]
